2f43b85932f3fca6aa507afbec4a5d0498c8256c
Kierath <kierath_dhugga@hotmail.co.uk>
2025-10-28 13:08:40 +0000

    updated vercel site as it wasn't reaching

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,8 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['https://coffee-shop-nu-pearl.vercel.app/'],
+    origin: ['https://coffee-shop-ohi5hcqnu-kieraths-projects.vercel.app/'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }));
 app.use(express.json());
